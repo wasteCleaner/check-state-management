b06111221fc7fb7d2f52affd7b373aae92f02ad9
Aleksandr Gorin <aleksandrgorin@MacBook-Pro-Aleksandr.local>
2018-11-22 23:02:07 +0200

    Installed react

--- extension.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var panels = chrome.devtools.panels;
-panels.create("Check State management", "", "panel.html", function (panel) { console.log(panel); });
--- extension.ts
+++
@@ -1,4 +0,0 @@
-declare const chrome: any;
-const panels = chrome.devtools.panels;
-
-panels.create("CheckState", "", "panel.html", (panel: any) => {console.log(panel)});
